4d0418c3c53ec9e58ad8eeadd70080166dd4ae10
Maciej Ruszczyk <m.ruszczyk.1@stud.elka.pw.edu.pl>
2018-02-27 08:14:57 +0100

    Initial commit

---
+++ src/main/java/pl/semantive/Application.java
@@ -0,0 +1,16 @@
+package pl.semantive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Maciek on 2018-02-26.
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
