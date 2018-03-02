3242ebc09b3645d501b747d3ae09df742d44a6d9
Maciej Ruszczyk <m.ruszczyk.1@stud.elka.pw.edu.pl>
2018-03-02 19:53:45 +0100

    Web Configuration and jpa dependencies

---
+++ src/main/java/pl/semantive/config/WebConfig.java
@@ -0,0 +1,46 @@
+package pl.semantive.config;
+
+import org.springframework.boot.autoconfigure.web.ErrorViewResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Collections;
+
+/**
+ * Created by Maciek on 2018-03-01.
+ */
+@Configuration
+public class WebConfig {
+
+    @Bean
+    ErrorViewResolver supportPathBasedLocationStrategyWithoutHashes() {
+        return (request, status, model) -> {
+            if (status == HttpStatus.NOT_FOUND) {
+                return new ModelAndView("index.html", Collections.emptyMap(), HttpStatus.OK);
+            } else {
+                return null;
+            }
+        };
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
