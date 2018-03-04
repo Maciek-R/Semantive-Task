e1c5dd49e056a5aa8fdd3142fcf6be7528938aca
Maciej Ruszczyk <m.ruszczyk.1@stud.elka.pw.edu.pl>
2018-03-04 18:40:54 +0100

    Added config class for mapping .html files. Added .js controller. Serving clients and new telephones. Added main page and page for adding new client.

---
+++ src/main/java/pl/semantive/config/ApplicationConfig.java
@@ -0,0 +1,27 @@
+package pl.semantive.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by Maciek on 2018-03-03.
+ */
+
+@Configuration
+public class ApplicationConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/addClient").setViewName("addClient.html");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    }
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        super.configurePathMatch(configurer);
+        configurer.setUseSuffixPatternMatch(false);
+    }
+}
---
+++ src/main/resources/static/js/controller.js
@@ -0,0 +1,128 @@
+var app = angular.module('app', []);
+app.controller('postcontroller', function($scope, $http, $location) {
+	$scope.submitForm = function(){
+		var url = $location.absUrl() + "professions/add";
+
+		var data = {
+            professionName: $scope.professionName
+        };
+
+        var successCallBack = function(response){
+            $scope.postResultMessage = response.data;
+        };
+        var errorCallBack = function(response){
+            $scope.postResultMessage = "Error with status: " +  response.statusText;
+        };
+
+		$http.post(url, data).then(successCallBack, errorCallBack);
+		$scope.professionName = "";
+	}
+
+	$scope.submitClientForm = function(){
+	        var mainUrl = $location.protocol() + "://"+
+                $location.host()+":"+$location.port();
+	        var url = mainUrl+"/clients/add";
+    		//var url = $location.absUrl() + "clients/add";
+
+    		var data = {
+                name: $scope.name,
+                age: $scope.age,
+                professionId: $scope.professionId
+            };
+
+            var successCallBack = function(response){
+                $scope.postResultMessage = response.data;
+                //$location.path(mainUrl + "/clientsList");
+                //window.location.assign(mainUrl + "/clientsList");
+                window.location.assign(mainUrl);
+            };
+            var errorCallBack = function(response){
+                $scope.postResultMessage = "Error with status: " +  response.statusText;
+            };
+
+    		$http.post(url, data).then(successCallBack, errorCallBack);
+    		$scope.professionName = "";
+    	}
+});
+
+app.controller('getcontroller', function($scope, $http, $location) {
+	$scope.getProfessions = function(){
+	    var url = $location.absUrl() + "professions";
+
+	    $http.get(url).then(function (response){
+	        $scope.professions = response.data
+	    }, function error(response){
+	        $scope.postResultMessage = "Error with status: " +  response.statusText;
+	    });
+	}
+
+	$scope.getClients = function(){
+    	    var url = $location.absUrl() + "clients";
+
+    	    $http.get(url).then(function (response){
+    	        $scope.clients = response.data
+    	    }, function error(response){
+    	        $scope.postResultMessage = "Error with status: " +  response.statusText;
+    	    });
+    }
+});
+
+app.controller('DataController', function($scope, $http, $location) {
+	$scope.getClients = function(){
+    	    //var url = $location.protocol() + "://"+
+    	    //$location.host()+":"+$location.port()+"/clientsListReq";
+    	    //console.log(url);
+            var url = $location.absUrl() + "clientsList";
+
+            var successCallBack = function(response){
+                $scope.clients = response.data;
+            };
+            var errorCallBack = function(response){
+                $scope.postResultMessage = "Error with status: " +  response.statusText;
+            };
+    	    $http.get(url).then(successCallBack, errorCallBack);
+    };
+
+    $scope.getTelephonesClient = function(client){
+         var mainUrl = $location.protocol() + "://"+
+            $location.host()+":"+$location.port();
+         var url = mainUrl+"/clients/"+client.id+"/clients_telephones";
+
+         $http.get(url).then(function (response){
+                $scope.clientTelephones = response.data
+         }, function error(response){
+                $scope.postResultMessage = "Error with status: " +  response.statusText;
+         });
+    };
+
+    $scope.submitAdditionalClientTelephone = function(){
+        var mainUrl = $location.protocol() + "://"+
+            $location.host()+":"+$location.port();
+        var url = mainUrl+"/clients_telephones/add";
+
+        var data = {
+            number: $scope.number,
+            clientId: $scope.selectedClient.id
+        };
+
+        console.log($scope.number);
+
+        var successCallBack = function(response){
+            $scope.postResultMessage = response.data;
+        };
+        var errorCallBack = function(response){
+            $scope.postResultMessage = "Error with status: " +  response.statusText;
+        };
+
+        $http.post(url, data).then(successCallBack, errorCallBack);
+        $scope.number = "";
+    };
+
+    $scope.isClientSelected = function(client){
+        return $scope.selectedClient == client;
+    };
+
+    $scope.selectClient = function(client){
+        $scope.selectedClient = client;
+    };
+});
