b81c1803970c70c536eb03b086dd7f30fbde06a2
Maciej Ruszczyk <m.ruszczyk.1@stud.elka.pw.edu.pl>
2018-03-05 13:24:18 +0100

    Error page - when url address is not correct.

--- src/main/java/pl/semantive/config/WebConfig.java
+++ src/main/java/pl/semantive/config/WebConfig.java
@@ -3,12 +3,14 @@ package pl.semantive.config;
 import org.springframework.boot.autoconfigure.web.ErrorViewResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Collections;
 
 /**
@@ -21,7 +23,7 @@ public class WebConfig {
     ErrorViewResolver supportPathBasedLocationStrategyWithoutHashes() {
         return (request, status, model) -> {
             if (status == HttpStatus.NOT_FOUND) {
-                return new ModelAndView("index.html", Collections.emptyMap(), HttpStatus.OK);
+                return new ModelAndView("error.html", Collections.emptyMap(), HttpStatus.OK);
             } else {
                 return null;
             }
