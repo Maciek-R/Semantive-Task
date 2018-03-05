52c96a762ebf91b144340b755ea1b8db70731de4
Maciej Ruszczyk <m.ruszczyk.1@stud.elka.pw.edu.pl>
2018-03-05 20:16:11 +0100

    Cleaning.

--- src/main/resources/static/js/controller.js
+++ src/main/resources/static/js/controller.js
@@ -1,7 +1,6 @@
 var app = angular.module('app', []);
-app.controller('postcontroller', function($scope, $http, $location) {
+app.controller('Postcontroller', function($scope, $http, $location) {
 	$scope.submitProfessionForm = function(){
-		//var url = $location.absUrl() + "professions/add";
         var mainUrl = $location.protocol() + "://"+
               $location.host()+":"+$location.port();
         var url = mainUrl+"/professions/add";
@@ -30,7 +29,6 @@ app.controller('postcontroller', function($scope, $http, $location) {
 	        var mainUrl = $location.protocol() + "://"+
                 $location.host()+":"+$location.port();
 	        var url = mainUrl+"/clients/add";
-    		//var url = $location.absUrl() + "clients/add";
 
     		var data = {
                 name: $scope.name,
@@ -64,39 +62,12 @@ app.controller('postcontroller', function($scope, $http, $location) {
         }
 });
 
-app.controller('getcontroller', function($scope, $http, $location) {
-	$scope.getProfessions = function(){
-	    var url = $location.absUrl() + "professions";
-
-	    $http.get(url).then(function (response){
-	        $scope.professions = response.data
-	    }, function error(response){
-	        $scope.postResultMessage = "Error with status: " +  response.statusText;
-	    });
-	}
-
-	$scope.getClients = function(){
-    	    var url = $location.absUrl() + "clients";
-
-    	    $http.get(url).then(function (response){
-    	        $scope.clients = response.data
-    	    }, function error(response){
-    	        $scope.postResultMessage = "Error with status: " +  response.statusText;
-    	    });
-    }
-});
-
 app.controller('DataController', function($scope, $http, $location) {
 	$scope.deleteClient = function(clientId){
-            //var url = $location.protocol() + "://"+
-            //$location.host()+":"+$location.port()+"/clientsListReq";
-            //console.log(url);
             var url = $location.absUrl() + "clients/delete/"+clientId;
 
             var successCallBack = function(response){
                 $scope.getClients();
-                //$scope.clients = response.data;
-                //console.log($scope.clients);
             };
             var errorCallBack = function(response){
                 $scope.postResultMessage = "Error with status: " +  response.statusText;
@@ -105,9 +76,6 @@ app.controller('DataController', function($scope, $http, $location) {
     };
 
 	$scope.getClients = function(){
-    	    //var url = $location.protocol() + "://"+
-    	    //$location.host()+":"+$location.port()+"/clientsListReq";
-    	    //console.log(url);
             var url = $location.absUrl() + "clientsInfo";
 
             var successCallBack = function(response){
